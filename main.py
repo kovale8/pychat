3e3a3d56d83f9b3148e8a3ba9a43587cc93e53dc
Gene Koval <genya@nova.local>
2018-11-29 11:45:03 -0500

    Added styling

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Chat App!'
+def root():
+    return render_template('root.html');
 
 
 if __name__ == '__main__':
